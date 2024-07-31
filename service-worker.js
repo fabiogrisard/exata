8318d36cba38fb9d0ea8bb5df0b7e0155ade5bb9
fabiogrisard <fabiogrisard@gmail.com>
2024-07-31 16:40:49 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('exata-cache').then(function(cache) {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
+                '/exata/',
+                '/exata/index.html',
+                '/exata/manifest.json',
                 'https://raw.githubusercontent.com/fabiogrisard/exata/main/icon-192x192.png',
                 'https://raw.githubusercontent.com/fabiogrisard/exata/main/icon-512x512.png'
                 // Adicione outros arquivos que você deseja armazenar em cache
